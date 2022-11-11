a101c3e62c7cd945c3cfdf2aabd2af906f969909
hizhy <105650344+hizhy@users.noreply.github.com>
2022-11-11 21:27:09 +0000

    Deploying to gh-pages from @ hizhy/goit-js-hw-09@3a7edfb812bc26ea1045870b00656bd41eb5f6c0 🚀

---
+++ 01-color-switcher.a2188714.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){t.disabled=!0,e=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),n.addEventListener("click",(function(){clearInterval(e),t.disabled=!1}));var e=null}();
+//# sourceMappingURL=01-color-switcher.a2188714.js.map
