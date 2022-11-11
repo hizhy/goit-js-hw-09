3a7edfb812bc26ea1045870b00656bd41eb5f6c0
Mykola_Kh <hizhym@gmail.com>
2022-11-11 23:26:30 +0200

    1st done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const startButRef = document.querySelector('button[data-start]');
+const stopButRef = document.querySelector('button[data-stop]');
+startButRef.addEventListener('click', onStartClick);
+stopButRef.addEventListener('click', onStopClick);
 
+let int = null;
+
+function onStartClick() {
+  startButRef.disabled = true;
+  int = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopClick() {
+  clearInterval(int);
+  startButRef.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
